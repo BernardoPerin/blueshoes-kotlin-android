e38018a12e17ad6b4c145ee7995154e92bf88e03
Bernardo Perin <bernardo_perin@hotmail.com>
2019-08-08 20:01:59 -0300

    Adicionando Fragmento da Tela Sobre

---
+++ app/src/main/java/bernardo/com/br/blueshoes/view/AboutFragment.kt
@@ -0,0 +1,28 @@
+package bernardo.com.br.blueshoes.view
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import bernardo.com.br.blueshoes.R
+
+
+class AboutFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater
+            .inflate(
+                R.layout.fragment_about,
+                container,
+                false
+            )
+    }
+
+
+}
