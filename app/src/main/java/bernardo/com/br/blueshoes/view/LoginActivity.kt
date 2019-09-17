3187fb367f36becac8a6a1ebc45a18df6c11409d
Bernardo Perin <bernardo_perin@hotmail.com>
2019-09-17 19:32:41 -0300

    Adicionando tela de login

---
+++ app/src/main/java/bernardo/com/br/blueshoes/view/LoginActivity.kt
@@ -0,0 +1,24 @@
+package bernardo.com.br.blueshoes.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import bernardo.com.br.blueshoes.R
+
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
