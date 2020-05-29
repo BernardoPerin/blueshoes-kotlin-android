fbf0daf03c75b650108ea40b9e9b1d945ac76b13
Bernardo Perin <bernardo_perin@hotmail.com>
2020-05-29 17:37:48 -0300

    Criar a UI de Configurações de Conta de Usuário

--- app/src/main/java/bernardo/com/br/blueshoes/view/ConfigProfileActivity.kt
+++ app/src/main/java/bernardo/com/br/blueshoes/view/ConfigProfileActivity.kt
@@ -1,19 +1,33 @@
 package bernardo.com.br.blueshoes.view
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import bernardo.com.br.blueshoes.R
 import bernardo.com.br.blueshoes.domain.User
 import bernardo.com.br.blueshoes.util.isValidEmail
 import bernardo.com.br.blueshoes.util.validate
+import com.blankj.utilcode.util.KeyboardUtils
+import com.blankj.utilcode.util.ScreenUtils
 import kotlinx.android.synthetic.main.content_config_profile.*
 import kotlinx.android.synthetic.main.content_forgot_password.*
+import kotlinx.android.synthetic.main.content_login.*
 import kotlinx.android.synthetic.main.info_block.*
+import kotlinx.android.synthetic.main.text_view_privacy_policy_login.*
 
 class ConfigProfileActivity :
-    FormActivity() {
+    FormActivity(),
+    KeyboardUtils.OnSoftInputChangedListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        KeyboardUtils.registerSoftInputChangedListener(
+            this,
+            this
+        )
 
         et_name.validate(
             {
@@ -34,19 +48,96 @@ class ConfigProfileActivity :
     override fun backEndFakeDelay(){
         backEndFakeDelay(
             false,
-            getString(R.string.invalid_login_email)
+            getString(R.string.invalid_config_profile)
         )
     }
 
     override fun blockFields( status: Boolean ){
-        et_email.isEnabled = !status
-        bt_recover_password.isEnabled = !status
+        iv_profile.isEnabled = !status
+        et_name.isEnabled = !status
+        bt_send_profile.isEnabled = !status
     }
 
     override fun isMainButtonSending( status: Boolean ){
-        bt_recover_password.text = if( status )
-            getString(R.string.recover_password_going)
-        else
-            getString(R.string.recover_password)
+        bt_send_profile.text =
+            if( status )
+                getString(R.string.config_profile_going)
+            else
+                getString(R.string.config_profile)
+    }
+
+    fun callGallery( view: View ){
+        Toast
+            .makeText( this, "TODO", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    override fun onDestroy() {
+        KeyboardUtils.unregisterSoftInputChangedListener(this)
+        super.onDestroy()
+    }
+
+    override fun onSoftInputChanged(height: Int) {
+        if( isAbleToCallChangeTargetViewConstraints() ){
+            changeTargetViewConstraints(
+                KeyboardUtils.isSoftInputVisible( this )
+            )
+        }
+    }
+
+    fun isAbleToCallChangeTargetViewConstraints()
+            = true
+
+    private fun changeTargetViewConstraints(
+        isKeyBoardOpened: Boolean
+    ){
+
+        val privacyId = tv_privacy_policy.id
+        val parent = tv_privacy_policy.parent as ConstraintLayout
+        val constraintSet = ConstraintSet()
+
+        constraintSet.constrainWidth(
+            privacyId,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
+        constraintSet.constrainHeight(
+            privacyId,
+            ConstraintLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        constraintSet.centerHorizontally(
+            privacyId,
+            ConstraintLayout.LayoutParams.PARENT_ID
+        )
+
+        if( isConstraintToSiblingView( isKeyBoardOpened ) ){
+            setConstraintsRelativeToSiblingView( constraintSet, privacyId)
+        }
+        else{
+            constraintSet.connect(
+                privacyId,
+                ConstraintLayout.LayoutParams.BOTTOM,
+                ConstraintLayout.LayoutParams.PARENT_ID,
+                ConstraintLayout.LayoutParams.BOTTOM
+            )
+        }
+
+        constraintSet.applyTo( parent )
+    }
+
+    fun isConstraintToSiblingView( isKeyBoardOpened: Boolean )
+            = isKeyBoardOpened || ScreenUtils.isLandscape()
+
+    fun setConstraintsRelativeToSiblingView(
+        constraintSet: ConstraintSet,
+        targetViewId: Int
+    ){
+        constraintSet.connect(
+            targetViewId,
+            ConstraintLayout.LayoutParams.BOTTOM,
+            tv_name.id,
+            ConstraintLayout.LayoutParams.TOP,
+            (30 * ScreenUtils.getScreenDensity().toInt())
+        )
     }
 }
