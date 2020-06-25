7ce9a13640678bdfadc844187435d2625ecf3294
Bernardo Perin <bernardo_perin@hotmail.com>
2020-06-25 17:28:48 -0300

    Telas de Configuração de E-mail e Senha

---
+++ app/src/main/java/bernardo/com/br/blueshoes/view/FormFragment.kt
@@ -0,0 +1,169 @@
+package bernardo.com.br.blueshoes.view
+
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.net.Uri
+import android.os.Bundle
+import android.os.SystemClock
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ImageSpan
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import androidx.fragment.app.Fragment
+
+import bernardo.com.br.blueshoes.R
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.content_form.*
+import kotlinx.android.synthetic.main.fragment_about.*
+import kotlinx.android.synthetic.main.proxy_screen.*
+
+
+abstract class FormFragment :
+    Fragment(),
+    TextView.OnEditorActionListener {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val viewContainer = inflater
+            .inflate(
+                R.layout.fragment_form,
+                container,
+                false
+            )
+
+        View.inflate(
+            activity,
+            getLayoutResourceID(),
+            viewContainer.findViewById(R.id.fl_form)
+        )
+
+        return viewContainer
+    }
+
+    abstract fun getLayoutResourceID() : Int
+
+    override fun onEditorAction(
+        view: TextView,
+        actionId: Int,
+        event: KeyEvent?): Boolean {
+
+        mainAction()
+        return false
+    }
+
+    protected fun showProxy( status: Boolean ){
+        fl_proxy_container.visibility =
+            if( status )
+                View.VISIBLE
+            else
+                View.GONE
+    }
+
+    protected fun snackBarFeedback(
+        viewContainer: View,
+        status: Boolean,
+        message: String
+    ){
+
+        val snackbar = Snackbar
+            .make(
+                viewContainer,
+                message,
+                Snackbar.LENGTH_LONG)
+
+        val iconResource =
+            if ( status )
+                R.drawable.ic_check_black_18dp
+            else
+                R.drawable.ic_close_black_18dp
+
+        val img = ResourcesCompat
+            .getDrawable(
+                resources,
+                iconResource,
+                null
+            )
+
+        img!!.setBounds(
+            0,
+            0,
+            img.intrinsicWidth,
+            img.intrinsicHeight
+        )
+
+        val iconColor =
+            if ( status )
+                ContextCompat.getColor(
+                    activity!!,
+                    R.color.colorNavButton
+                )
+            else
+                Color.RED
+
+        img.setColorFilter( iconColor, PorterDuff.Mode.SRC_ATOP )
+
+        val spannedTxt = SpannableString("     $message")
+        spannedTxt.setSpan(
+            ImageSpan( img, ImageSpan.ALIGN_BOTTOM),
+            0,
+            1,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        val textView = snackbar
+            .view
+            .findViewById<TextView>( com.google.android.material.R.id.snackbar_text )
+
+        textView.setText( spannedTxt, TextView.BufferType.SPANNABLE)
+
+        snackbar.show()
+    }
+
+    fun mainAction( view: View? = null ){
+        blockFields( true )
+        isMainButtonSending( true )
+        showProxy( true )
+        backEndFakeDelay()
+    }
+
+    abstract fun backEndFakeDelay() : Unit
+
+    abstract fun blockFields( status: Boolean )
+
+    abstract fun isMainButtonSending( status: Boolean )
+
+    protected fun backEndFakeDelay(
+        statusAction: Boolean,
+        feesbackMessage: String
+    ){
+        Thread{
+            kotlin.run {
+                SystemClock.sleep(1000)
+
+                activity!!.runOnUiThread{
+                    blockFields( false )
+                    isMainButtonSending( false )
+                    showProxy( false )
+
+                    snackBarFeedback(
+                        fl_form_container,
+                        statusAction,
+                        feesbackMessage
+                    )
+                }
+            }
+        }.start()
+    }
+}
